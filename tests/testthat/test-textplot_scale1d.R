3dd89b61dd71ffa0c186db6e763873121bb18bd3
kbenoit <kbenoit@lse.ac.uk>
2020-12-15 17:18:29 +0000

    Update tests for r-release
    Fixes https://github.com/quanteda/quanteda.textmodels/issues/41

--- tests/testthat/test-textplot_scale1d.R
+++ tests/testthat/test-textplot_scale1d.R
@@ -19,7 +19,7 @@ test_that("test textplot_scale1d wordfish in the most basic way", {
     p2 <- textplot_scale1d(wf, margin = "features", sort = FALSE)
     p1$plot_env <- NULL
     p2$plot_env <- NULL
-    # expect_equivalent(p1, p2)
+    expect_equal(p1, p2, check.environment = FALSE)
 })
 
 test_that("test textplot_scale1d wordscores in the most basic way", {
@@ -48,7 +48,7 @@ test_that("test textplot_scale1d wordscores in the most basic way", {
     p2 <- textplot_scale1d(ws, margin = "features", sort = FALSE)
     p1$plot_env <- NULL
     p2$plot_env <- NULL
-    # expect_equivalent(p1, p2)
+    expect_equal(p1, p2, check.environment = FALSE)
 
     expect_error(
         textplot_scale1d(ws, margin = "documents"),
@@ -58,66 +58,6 @@ test_that("test textplot_scale1d wordscores in the most basic way", {
         suppressWarnings(textplot_scale1d(predict(ws), margin = "features")),
         "This margin can only be run on a fitted wordscores object"
     )
-})
-
-test_that("test textplot_affinity", {
-    af <- quanteda.textmodels::textmodel_affinity(data_dfm_lbgexample, y = c("L", NA, NA, NA, "R", NA))
-    afpred <- predict(af)
-    # expect_silent(textplot_influence(influence(afpred)))
-    # expect_silent(textplot_influence(summary(influence(afpred))))
-})
-
-test_that("multiple patterns display correctly in textplot_kwic", {
-    skip("For interactive visual inspection only")
-    toks <- tokens(c(alpha1 = paste(letters, collapse = " "),
-                     alpha2 = paste(LETTERS, collapse = " ")))
-
-    kwic_char_f <- kwic(toks, "f", window = 3)
-    kwic_char_u <- kwic(toks, "u", window = 3)
-    kwic_char_uf <- kwic(toks, c("u", "f"), window = 3)
-    kwic_char_fu <- kwic(toks, c("f", "u"), window = 3)
-    kwic_dict_u <- kwic(toks, dictionary(list(ukey = "u")), window = 3)
-    kwic_dict_f <- kwic(toks, dictionary(list(fkey = "f")), window = 3)
-    kwic_dict_uf <- kwic(toks, dictionary(list(ukey = "u", fkey = "f")), window = 3)
-    kwic_dict_fu <- kwic(toks, dictionary(list(fkey = "f", ukey = "u")), window = 3)
-    kwic_dict_uf_jm <- kwic(toks, dictionary(list(ufkey = c("u", "f"),
-                                                  jmkey = c("j", "m"))), window = 3)
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_char_f, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_char_u, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_char_u, kwic_char_f, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_dict_f, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_dict_u, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_dict_u, kwic_dict_f, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_dict_f, kwic_dict_u, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_dict_uf, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_dict_fu, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_char_uf, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_char_fu, scale = "absolute")
-
-    # warning free: ✓  label order correct: ✓  plot order correct: ✓
-    textplot_xray(kwic_dict_uf_jm, scale = "absolute")
 })
 
 dev.off()
